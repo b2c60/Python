0ab03c4df0f7c0c6a90d1b700214b2261eba8875
b2c60 <weib825@gmail.com>
2015-07-27 17:06:20 +0800

    modified foo() def

--- ch06_mutable_deafault.py
+++ ch06_mutable_deafault.py
@@ -4,9 +4,11 @@ def color(red, green, blue, alpha=ALPHA_DEFAULT):
     # ...
     print (red, green, blue, alpha)
 
-def foo(x, y=[]):
+def foo(x, y=None):
+    if y is None:
+        y = []
     y.append(x)
     print(y)
 foo(1)
 foo(2)
-foo(3)
+foo(3, [90, 91])
