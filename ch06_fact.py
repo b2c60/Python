acbca43b78cf8fafb752de644527a79a71a89dad
b2c60 <weib825@gmail.com>
2015-08-06 18:08:40 +0800

    add ch06_fact, ch06_gcd, ch06_perm

---
+++ ch06_fact.py
@@ -0,0 +1,12 @@
+def fact_i(n):
+    result = 1
+    for i in range(1, n+1):
+        result *= i
+    return result
+#####
+def fact_r(n):
+    if n == 1 or n == 0:
+        return 1
+    else:
+        return n * fact_r(n-1)
+        
